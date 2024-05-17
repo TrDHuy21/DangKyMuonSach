0f616d64895a883351d9a8fee13f750a53a98665
Tran Duc Huy <vlsvhuy@gmail.com>
2024-05-17 10:34:53 +0700

    WIP on entity_db
    test connect

--- app/src/androidTest/java/nhom2/dangkymuonsach/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/nhom2/dangkymuonsach/ExampleInstrumentedTest.java
@@ -2,13 +2,21 @@ package nhom2.dangkymuonsach;
 
 import android.content.Context;
 
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import nhom2.dangkymuonsach.dao.UserDAO;
+import nhom2.dangkymuonsach.database.AppDatabase;
+import nhom2.dangkymuonsach.entity.UserEntity;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +25,36 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private AppDatabase appDatabase;
+    private UserDAO userDAO;
+    Context appContext;
+    @Before
+    public void setup() {
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        appDatabase = Room.inMemoryDatabaseBuilder(
+                appContext,
+                AppDatabase.class
+        ).allowMainThreadQueries().build();
+        userDAO = appDatabase.getUserDAO();
+    }
+
+    @After
+    public void cleanup() {
+        appDatabase.close();
+    }
     @Test
     public void useAppContext() {
+        UserEntity entity = new UserEntity();
+        entity.setTen("Test");
+        userDAO.insert(entity);
+
+        UserEntity retrievedEntity = userDAO.getById(entity.getId());
+        assertNotNull(retrievedEntity);
+        assertEquals(entity.getTen(), retrievedEntity.getTen());
+        assertEquals(4, 2 + 2);
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+
         assertEquals("nhom2.dangkymuonsach", appContext.getPackageName());
     }
 }
