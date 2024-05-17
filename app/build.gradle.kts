b625ca4a49c1598968ce8909c3ef10ff7501b471
Tran Duc Huy <vlsvhuy@gmail.com>
2024-05-17 09:23:24 +0700

    add dependencies lombok and room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    kotlin("kapt") version "1.9.24"
 }
 
 android {
@@ -43,4 +44,31 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("org.projectlombok:lombok:1.18.0")
+    annotationProcessor ("org.projectlombok:lombok:1.18.0")
+
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    // To use Kotlin annotation processing tool (kapt)
+    implementation ("android.arch.persistence.room:runtime:$room_version")
+    annotationProcessor ("android.arch.persistence.room:compiler:$room_version")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
 }
