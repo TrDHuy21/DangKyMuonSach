0f91672f989eb41c0bc5716db7688913ea12b1df
Tran Duc Huy <vlsvhuy@gmail.com>
2024-05-17 23:21:59 +0700

    code AppDatabase

--- app/src/main/java/nhom2/dangkymuonsach/database/AppDatabase.java
+++ app/src/main/java/nhom2/dangkymuonsach/database/AppDatabase.java
@@ -1,12 +1,31 @@
 package nhom2.dangkymuonsach.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import nhom2.dangkymuonsach.dao.UserDAO;
+import nhom2.dangkymuonsach.database.dao.UserDAO;
 import nhom2.dangkymuonsach.entity.UserEntity;
 
 @Database(entities = {UserEntity.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+    static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "Sample.db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
     public abstract UserDAO getUserDAO();
+
 }
