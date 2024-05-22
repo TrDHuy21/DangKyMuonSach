5ffb87b790b8096b44186ae0ef6e1c819618f9d7
lamnguyenthien <thienlamntl22@gmail.com>
2024-05-22 10:43:10 +0700

    login_display

---
+++ app/src/main/java/nhom2/dangkymuonsach/SignupActivity.java
@@ -0,0 +1,14 @@
+package nhom2.dangkymuonsach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
